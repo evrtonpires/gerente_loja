0ef87ea4cb15c3a091cb1cf0691857ba363556f3
Everton Pires <epe.masork@hotmail.com>
2019-07-30 09:55:46 -0300

    Criando InputField para o Formulario da LoginScreen e Finalizando Tela Inicial

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,29 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:gerente_loja/widgets/input_field.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
-
+//-----------------------------------------------------------------------------
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[850],
-      body: SingleChildScrollView(
-        child: Container(
-          margin: EdgeInsets.all(16),
-          child: Column(
-            children: <Widget>[
-              Icon(
-                Icons.store_mall_directory,
-                color: Colors.pinkAccent,
-                size: 160.0,
-              )
-            ],
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Container(),
+          SingleChildScrollView(
+            child: Container(
+              margin: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Icon(
+                    Icons.store_mall_directory,
+                    color: Colors.pinkAccent,
+                    size: 160.0,
+                  ),
+                  InputField(
+                    icon: Icons.person_outline,
+                    hint: "usuario",
+                    obscure: false,
+                  ),
+                  InputField(
+                    icon: Icons.lock_outline,
+                    hint: "senha",
+                    obscure: true,
+                  ),
+                  SizedBox(
+                    height: 32,
+                  ),
+                  SizedBox(
+                    height: 50.0,
+                    child: RaisedButton(
+                        color: Colors.pinkAccent,
+                        child: Text("ENTRAR"),
+                        textColor: Colors.white,
+                        onPressed: () {}),
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
 }
+//-----------------------------------------------------------------------------
---
+++ lib/widgets/input_field.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+//Tela do Formulario do Login(usuario e senha).
+
+class InputField extends StatelessWidget {
+//-----------------------------------------------------------------------------
+//Atributos
+  final IconData icon;
+  final String hint;
+  final bool obscure;
+
+//-----------------------------------------------------------------------------
+  //Construtor
+  InputField({this.icon, this.hint, this.obscure});
+
+//-----------------------------------------------------------------------------
+  //Metodos
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+          icon: Icon(
+            icon,
+            color: Colors.white,
+          ),
+          hintText: hint,
+          hintStyle: TextStyle(color: Colors.white),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.pinkAccent),
+          ),
+          contentPadding:
+              EdgeInsets.only(left: 5, right: 30, bottom: 30, top: 30)),
+      style: TextStyle(
+        color: Colors.white,
+      ),
+      obscureText: obscure,
+    );
+  }
+}
+//-----------------------------------------------------------------------------
