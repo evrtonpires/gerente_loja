769e51977284d16a5b2f742f33c2c08005a5a420
evrtonpires <epe.masork@gmail.com>
2019-07-30 15:50:02 -0300

    Obtendo Gasto Total

--- lib/blocs/user_bloc.dart
+++ lib/blocs/user_bloc.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:rxdart/rxdart.dart';
 
 class UserBloc extends BlocBase {
-
 //-----------------------------------------------------------------------------
   //Controlador
   final _userController = BehaviorSubject();
@@ -33,25 +32,58 @@ class UserBloc extends BlocBase {
     //sempre que tiver alteração no banco,ira chamar esta stream,e pode obter as modificações na colecao users
     _firestore.collection("users").snapshots().listen((snapshot) {
       snapshot.documentChanges.forEach((change) {
-        String uid = change.document
-            .documentID; //ID do usuario que os dados foram modificados
+        String uid = change
+            .document.documentID; //ID do usuario que os dados foram modificados
 
         switch (change.type) {
           case DocumentChangeType.added:
             _users[uid] =
                 change.document.data; //armazena os dados do usuario ao map
-
+            _subscribeToOrders(uid);
             break;
           case DocumentChangeType.modified:
             _users[uid].addAll(
                 change.document.data); //armazena as modificaçoes ao mapa
+            _userController.add(_users.values.toList());
             break;
           case DocumentChangeType.removed:
             _users.remove(uid);
+            _unsubscribeToOrders(uid);
+            _userController.add(_users.values.toList());
             break;
         }
       });
     });
+  }
+
+  void _subscribeToOrders(String uid) {
+    _users[uid]["subscription"] = _firestore
+        .collection("users")
+        .document(uid)
+        .collection("orders")
+        .snapshots()
+        .listen((orders) async {
+      int numOrders = orders.documents.length;
+
+      double money = 0.0;
+
+      for (DocumentSnapshot d in orders.documents) {
+        DocumentSnapshot order =
+        await _firestore.collection("orders").document(d.documentID).get();
+
+        if (order.data == null) continue;
+
+        money += order.data["totalPrice"];
+      }
+
+      _users[uid].addAll({"money": money, "orders": numOrders});
+
+      _userController.add(_users.values.toList());
+    });
+  }
+
+  void _unsubscribeToOrders(String uid) {
+    _users[uid]["subscription"].cancel();
   }
 
 //-----------------------------------------------------------------------------
