cb4afe2f03fc9aef26f306a1c045e87b89199f4e
evrtonpires <epe.masork@gmail.com>
2019-07-30 15:33:02 -0300

    Observando Mudanças na Colecao

--- lib/blocs/user_bloc.dart
+++ lib/blocs/user_bloc.dart
@@ -1,8 +1,58 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rxdart/rxdart.dart';
 
 class UserBloc extends BlocBase {
+
+//-----------------------------------------------------------------------------
+  //Controlador
+  final _userController = BehaviorSubject();
+
+//-----------------------------------------------------------------------------
+  //Map
+  Map<String, Map<String, dynamic>> _users = {};
+
+//-----------------------------------------------------------------------------
+  //Firestore para acessar o firestore mais rapido
+  Firestore _firestore = Firestore.instance;
+
+//-----------------------------------------------------------------------------
+  //Construtor
+  UserBloc() {
+    _addUsersListener();
+  }
+
+//-----------------------------------------------------------------------------
+  //Metodos
   @override
   void dispose() {
-    // TODO: implement dispose
+    _userController.close();
   }
+
+  void _addUsersListener() {
+    //sempre que tiver alteração no banco,ira chamar esta stream,e pode obter as modificações na colecao users
+    _firestore.collection("users").snapshots().listen((snapshot) {
+      snapshot.documentChanges.forEach((change) {
+        String uid = change.document
+            .documentID; //ID do usuario que os dados foram modificados
+
+        switch (change.type) {
+          case DocumentChangeType.added:
+            _users[uid] =
+                change.document.data; //armazena os dados do usuario ao map
+
+            break;
+          case DocumentChangeType.modified:
+            _users[uid].addAll(
+                change.document.data); //armazena as modificaçoes ao mapa
+            break;
+          case DocumentChangeType.removed:
+            _users.remove(uid);
+            break;
+        }
+      });
+    });
+  }
+
+//-----------------------------------------------------------------------------
 }
