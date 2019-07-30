eb21be29ceed8e0b80668fde2d6fde1831aff5d9
Everton Pires <epe.masork@hotmail.com>
2019-07-30 09:32:08 -0300

    Criando Tela Inicial e Migrando para Android X

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gerente_loja/screens/login_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: LoginScreen(),
     );
   }
 }
